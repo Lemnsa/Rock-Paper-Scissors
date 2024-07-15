5383ee10577519c032c5a48aa2f4d5f068a951c0
Lemnsa <legeryennyuy@gmail.com>
2024-07-15 14:04:06 +0100

    Update getComputerChoice() return random value
    Call function to display the computer choice.

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
 // function to get computer choice
 function getComputerChoice() {
     let values = ["rock","paper","scissors"];
+    let randomIndex = Math.floor(Math.random() * 3); // select a random number between 0 and 3.
+    // Math.floor() rounds down the decimal values
+    let computerChoice = values[randomIndex];
+    return computerChoice;
 }
+
+getComputerChoice();
